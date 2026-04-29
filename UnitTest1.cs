bf0bbc03ab7aab78207172a4d873a11f8a117090
Sergio Maccari <s.maccari@perfexia.it>
2026-04-29 08:37:37 +0200

    test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -45,7 +45,7 @@ namespace PlaywrightNET9
             await page.WaitForTimeoutAsync(3000);
             await page.ScreenshotAsync(new()
             {
-                Path = "./../../../HomeScreenshot.jpg",
+                Path = "./../../../Screenshot/HomeScreenshot.jpg",
                 //string fileProcessicsv = "./../../../Processi.csv";
                 //C:\Lab2026\PlaywrightNET9\PlaywrightNET9\UnitTest1.cs
                 FullPage = true
